0e4f9957ca873c01b1e46ca71e2c7d81d8d0ac7b
tkcode <tkcode.dev@gmail.com>
2020-03-21 17:44:20 -0400

    Add Async, try-catch, to fetch current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,7 +7,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -19,22 +18,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     super.deactivate();
   }
 
-  void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
-          },
+          onPressed: () {},
           child: Text('button'),
         ),
       ),
     );
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude.toString() + " : " + location.longitude.toString());
   }
 }
